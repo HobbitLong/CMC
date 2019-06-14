de36d6f2f1e8d1b3a2b1068003c1c664be050398
HobbitLong <yonglong@mit.edu>
2019-06-14 14:56:22 -0700

    update

--- models/resnet.py
+++ models/resnet.py
@@ -106,20 +106,23 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, low_dim=128, in_channel=3):
+    def __init__(self, block, layers, low_dim=128, in_channel=3, is_half=False):
         self.inplanes = 64
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channel, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
+
+        self.base = 32 if is_half else 64
+
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer1 = self._make_layer(block, self.base, layers[0])
+        self.layer2 = self._make_layer(block, self.base * 2, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, self.base * 4, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, self.base * 8, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, low_dim)
+        self.fc = nn.Linear(self.base * 8 * block.expansion, low_dim)
         self.l2norm = Normalize(2)
 
         for m in self.modules():
@@ -258,150 +261,18 @@ def resnet152(pretrained=False, **kwargs):
     return model
 
 
-class ResNetHalf(nn.Module):
-
-    def __init__(self, block, layers, low_dim=128, in_channel=3):
-        self.inplanes = 64
-        super(ResNetHalf, self).__init__()
-        self.conv1 = nn.Conv2d(in_channel, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64//2, layers[0])
-        self.layer2 = self._make_layer(block, 128//2, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256//2, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512//2, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear((512//2) * block.expansion, low_dim)
-        self.l2norm = Normalize(2)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        x = self.l2norm(x)
-
-        return x
-
-    def compute_feat(self, x, layer):
-        if layer <= 0:
-            return x
-
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        if layer == 1:
-            return x
-
-        x = self.layer1(x)
-        if layer == 2:
-            return x
-
-        x = self.layer2(x)
-        if layer == 3:
-            return x
-
-        x = self.layer3(x)
-        if layer == 4:
-            return x
-
-        x = self.layer4(x)
-        if layer == 5:
-            return x
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        if layer == 6:
-            return x
-
-        x = self.fc(x)
-        x = self.l2norm(x)
-
-        return x
-
-
-def resnet18half(pretrained=False, **kwargs):
-    """Constructs a ResNet-18 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNetHalf(BasicBlock, [2, 2, 2, 2], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
-    return model
-
-
-def resnet50half(pretrained=False, **kwargs):
-    """Constructs a ResNet-50 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNetHalf(Bottleneck, [3, 4, 6, 3], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
-    return model
-
-
-def resnet101half(pretrained=False, **kwargs):
-    """Constructs a ResNet-101 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNetHalf(Bottleneck, [3, 4, 23, 3], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
-    return model
-
-
-class ResNet_CMC_V1(nn.Module):
+class ResNetV1(nn.Module):
     def __init__(self, name='resnet50'):
-        super(ResNet_CMC_V1, self).__init__()
+        super(ResNetV1, self).__init__()
         if name == 'resnet50':
-            self.l_to_ab = resnet50half(in_channel=1)
-            self.ab_to_l = resnet50half(in_channel=2)
+            self.l_to_ab = resnet50(in_channel=1, is_half=True)
+            self.ab_to_l = resnet50(in_channel=2, is_half=True)
         elif name == 'resnet18':
-            self.l_to_ab = resnet18half(in_channel=1)
-            self.ab_to_l = resnet18half(in_channel=2)
+            self.l_to_ab = resnet18(in_channel=1, is_half=True)
+            self.ab_to_l = resnet18(in_channel=2, is_half=True)
         elif name == 'resnet101':
-            self.l_to_ab = resnet101half(in_channel=1)
-            self.ab_to_l = resnet101half(in_channel=2)
+            self.l_to_ab = resnet101(in_channel=1, is_half=True)
+            self.ab_to_l = resnet101(in_channel=2, is_half=True)
         else:
             raise NotImplementedError('model {} is not implemented'.format(name))
 
@@ -419,9 +290,9 @@ class ResNet_CMC_V1(nn.Module):
         return feat
 
 
-class ResNet_CMC_V2(nn.Module):
+class ResNetV2(nn.Module):
     def __init__(self, name='resnet50'):
-        super(ResNet_CMC_V2, self).__init__()
+        super(ResNetV2, self).__init__()
         if name == 'resnet50':
             self.l_to_ab = resnet50(in_channel=1)
             self.ab_to_l = resnet50(in_channel=2)
